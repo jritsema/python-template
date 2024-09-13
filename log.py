d151916fd72052eea6fe7fa6516fea27f174a85a
John Ritsema <john_ritsema@yahoo.com>
2024-09-13 21:40:05 +0000

    adds logging boilerplate

---
+++ log.py
@@ -0,0 +1,26 @@
+import logging
+import json
+
+
+def debug(obj):
+    """
+    Print a JSON representation of the given object to the console, at debug level.
+    """
+    if logging.getLogger().level == logging.DEBUG:
+        print(json.dumps(obj, indent=2, default=str))
+
+
+def info(obj):
+    """
+    Print a JSON representation of the given object to the console, at info level.
+    """
+    if logging.getLogger().level == logging.INFO:
+        print(json.dumps(obj, indent=2, default=str))
+
+
+def warn(obj):
+    """
+    Print a JSON representation of the given object to the console, at warn level.
+    """
+    if logging.getLogger().level == logging.WARNING:
+        print(json.dumps(obj, indent=2, default=str))
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import logging
+from log import info
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
 def main():
-    print("hello world")
+    logging.info("hello world")
+    info({"hello": "world"})
 
 
 if __name__ == "__main__":
